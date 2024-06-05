787d28a742a2d00ddb154291ee0b38ddb759a612
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-06-05 15:33:10 +0200

    add posts with JS

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-async function includeHTML() {
-    let includeElements = document.querySelectorAll("[w3-include-html]");
-    for (let i = 0; i < includeElements.length; i++) {
-      const element = includeElements[i];
-      file = element.getAttribute("w3-include-html"); // "includes/header.html"
-      let resp = await fetch(file);
-      if (resp.ok) {
-        element.innerHTML = await resp.text();
-      } else {
-        element.innerHTML = "Page not found";
-      }
-    }
+function showPosts() {
+
+  let post = document.getElementById(`post`);
+  post.innerHTML = "";
+
+  for (let i = 0; i < content.length; i++) {
+    const posts = content[i];
+    let addition = posts.likes + posts.mylike[0]
+    post.innerHTML += /* html */ `
+    <div class="post">
+      <div class="p-head">
+        <img class="p-profilpic" src="${posts["profilpic"]}" alt="Profilbild">
+          <div class="p-title">
+            <h3>${posts["name"]}</h3>
+            <span>${posts["location"]}</span>
+          </div>
+      </div>
+      <img src="${posts["image"]}" alt="Inhaltsbild">
+      <div>
+        <button>like</button>
+        <button>allcomments</button>
+        <button>share</button>
+      </div>
+      <div>gefällt ${addition}  anderen</div>
+      
+    </div>`;
   }
+}
