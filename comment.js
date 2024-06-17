a36604b81c8eb2685ae5c487d1506b1fdea4c394
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-06-17 14:42:43 +0200

    hide function and style v0.8

--- comment.js
+++ comment.js
@@ -1,21 +1,40 @@
 function editComment(i) {
-    let comment = document.getElementById(`comments${i}`);
-    comment.innerHTML = "";
-    const dialog = content[i];
-    for (let j = 0; j < dialog["comments"].length; j++) {
-      const mycomment = dialog["comments"][j];
-      const username = dialog["user"][j];
-      comment.innerHTML += /* html */ `<div class="comment"><b>${username}:</b> ${mycomment}</div>`;
-    }
-    saveLocal();
+  let comment = document.getElementById(`comments${i}`);
+  comment.innerHTML = "";
+  let dialog = content[i];
+  for (let j = 0; j < dialog["comments"].length; j++) {
+    let mycomment = dialog["comments"][j];
+    let username = dialog["user"][j];
+    comment.innerHTML += /* html */ `<div class="comment"><b>${username}:</b> ${mycomment}</div>`;
   }
-  
-  function addComment(i) {
-    let myuser = "philipplötzsch";
-    let input = document.getElementById(`inputComment${i}`);
-    content[i]["user"].push(myuser);
-    content[i]["comments"].push(input.value);
-    editComment(i);
-    input.value = "";
-  }
-  
+  saveLocal();
+  commentsCount(i);
+}
+
+function addComment(i) {
+  let myuser = "philipplötzsch";
+  let input = document.getElementById(`inputComment${i}`);
+  content[i]["user"].push(myuser);
+  content[i]["comments"].push(input.value);
+  editComment(i);
+  input.value = "";
+}
+
+function commentsCount(i) {
+  let count = content[i]["comments"].length;
+  document.getElementById(`commentsCount${i}`).innerHTML = `(${count})`;
+}
+
+function showAllComments(i){
+  let showComments= document.getElementById(`comments${i}`)
+  let showCommentsBtn = document.getElementById(`showCommentsBtn${i}`);
+  showComments.classList.toggle("d-none");
+
+  if (showComments.classList.contains("d-none")) {
+    showCommentsBtn.innerHTML = `
+        <img src="./assets/img/allcomments.png" alt="all comments">`;
+} else {
+    showCommentsBtn.innerHTML = `<img src="./assets/img/showallcomments.png" alt="all comments">`;
+}
+
+}
--- templates.js
+++ templates.js
@@ -11,16 +11,16 @@ function contentHTML(i, posts) {
       <img class="postimage" src="${posts["image"]}" alt="Inhaltsbild">
       <div class="postbuttons">
         <button onclick="likePost(${i})" title="like" id="likeButton${i}"><img src="./assets/img/heartempty.svg" alt="heart"></button>
-        <button title="Zeige alle Kommentare"><img src="./assets/img/allcomments.png" alt="all comments"></button>
+        <button onclick="showAllComments(${i})" title="Zeige alle Kommentare" id="showCommentsBtn${i}"><img src="./assets/img/allcomments.png" alt="all comments"></button>
         <button title="teilen"><img src="./assets/img/share.png" alt=""></button>
       </div> 
       <p> <div id="like${i}"></div> </p>
       <p class="post-description"> <b> ${posts["description"]} </b></p>
       <div class="hashtags" id="hashtags${i}"></div>
-      <p> <h4>Kommentare:</h4> <br> 
-      <div id="comments${i}" class="comments"></div> </p>
+      <p> <h4>Kommentare <span id="commentsCount${i}" class="comments-count"></span>:</h4> <br> 
+      <div id="comments${i}" class="comments d-none"></div> </p>
       <form id="addComment" onsubmit="addComment(${i}); return false">
-      <input type="text" id="inputComment${i}" class="input-comment" placeholder="Kommentar" required> <button type="submit" class="btn-post">posten</button>
+      <input type="text" id="inputComment${i}" class="input-comment" placeholder="Hier Kommentar eingeben" required> <button type="submit" class="btn-post">posten</button>
       </form>
     </div>`;
 }
