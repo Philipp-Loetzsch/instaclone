b46a65017da53abd7e1afc82073815a6106e1435
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-06-18 15:32:54 +0200

    stories add smooth behavior

--- javascript/scrollstories.js
+++ javascript/scrollstories.js
@@ -2,7 +2,7 @@ function scrollStoriesForward() {
   document.getElementById("storiesBack").classList.remove("d-none");
   let storiepics = document.getElementById("storiepics");
   let scrollAmount = storiepics.clientWidth * 0.4;
-  storiepics.scrollLeft += scrollAmount;
+  storiepics.scrollBy({ left: scrollAmount, behavior: "smooth" });
   if (storiepics.scrollLeft + storiepics.clientWidth >= storiepics.scrollWidth
   ) {
     document.getElementById(`storiesForward`).classList.add("invalid");
@@ -13,7 +13,7 @@ function scrollStoriesBackward() {
   document.getElementById(`storiesForward`).classList.remove("invalid");
   let storiepics = document.getElementById("storiepics");
   let scrollAmount = storiepics.clientWidth * 0.4;
-  storiepics.scrollLeft -= scrollAmount;
+  storiepics.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   if (storiepics.scrollLeft <= 0) {
     document.getElementById(`storiesBack`).classList.add("d-none");
   }
